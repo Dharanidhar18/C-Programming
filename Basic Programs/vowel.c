e8ef173d90afc18c235458ea7758c475f2c8ed7f
BEERE DHARANIDHAR <93582679+Dharanidhar18@users.noreply.github.com>
2022-03-19 15:18:48 +0530

    C Programming

---
+++ Basic Programs/vowel.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{  
+    char s[11];
+    printf("ENTER THE NAME\t");
+    scanf("%s",s); 
+    int i = 0;   
+    int vowel= 0;  
+    while(i<11)  
+    {  
+        if(s[i]=='a'|| s[i]=='e'|| s[i]=='i'|| s[i]=='u'|| s[i]=='o')  
+        {  
+            vowel++;  
+        }  
+        i++;  
+    }  
+    printf("The number of vowels present in a word is:%d",vowel);  
+}  
